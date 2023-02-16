4c5edcae4a7d5c61e3c0e4c3b5a82d9097fc0bbd
khatia1999 <khatiaormotsadze@gmail.com>
2023-02-17 00:14:08 +0400

    Add images/ file

---
+++ index.js
@@ -0,0 +1,32 @@
+const hourEl   = document.getElementById("Hour");
+const minuteEl = document.getElementById("Minutes");
+const secondsEl= document.getElementById("Seconds");
+const amElement= document.getElementById("ampm");
+
+function  updateClock(){
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
+
+    if(h > 12){
+        h = h - 12;
+        ampm = "PM"
+    } 
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+
+    hourEl.innerText = h; 
+    minuteEl.innerText = m;
+    secondsEl.innerText = s;
+    amElement.innerText = ampm;
+    setTimeout(()=> {
+    updateClock()
+    }, 1000)
+}
+updateClock();
+
+
